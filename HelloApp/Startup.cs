280933ff5e78897f0e337dd3018936412c930d20
Budya <budya201@gmail.com>
2018-08-28 17:00:56 +0300

    Middleware

--- HelloApp/Startup.cs
+++ HelloApp/Startup.cs
@@ -11,27 +11,17 @@ namespace HelloApp
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            int x = 5;
-            int y = 8;
-            int z = 0;
-            app.Use(async (context, next) =>
-            {
-                z = x * y;
-                await next.Invoke();
-            });
+            app.UseToken();
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync($"x * y = {z}");
+                await context.Response.WriteAsync("Hello World");
             });
         }
     }
